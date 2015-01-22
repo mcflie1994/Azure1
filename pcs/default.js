1d5537bf8c4807367a2dcccb3543977114ef379b
Martin McNichols <mcflie1994@gmail.com>
2015-01-21 21:04:45 -0900

    fixed password in VC for PCS

--- pcs/default.js
+++ pcs/default.js
@@ -3,9 +3,9 @@
  */
 $(document).ready(function() {
     var pass = prompt('Please enter the password to access the ' + $('title').html());
-    if (pass != 'vcpass') {
+    if (pass != $('#pass').html()) {
         window.location.replace('index.html');
-    };
+    }
     $('.button').on('mousedown', function() {
         $(this).css('border', '3px inset lightgray');
         $(this).on('mouseup', function() {
