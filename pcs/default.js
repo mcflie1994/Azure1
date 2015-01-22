33c588a17065eaf370e30e6becd157a27a057984
Martin McNichols <mcflie1994@gmail.com>
2015-01-22 12:21:15 -0900

    Added 2ndary nav to PCS, fixed password implementation, added scripts to all pages

--- pcs/default.js
+++ pcs/default.js
@@ -2,11 +2,19 @@
  * Created by mcflie1994 on 1/21/15.
  */
 $(document).ready(function() {
-    if ($('#pass').html() != "" || $('#pass').html() !== undefined) {
+    if ($('#pass').html() !== "" && $('#pass').html() !== undefined) {
         var pass = prompt('Please enter the password to access the ' + $('title').html());
         if (pass != $('#pass').html()) {
             window.location.replace('index.html');
         }
+    }
+    if ($('#nav2').html() !== "" && $('#nav2').html() !== undefined) {
+        $('#nav2').animate({marginTop: 0}, 1500, 'linear');
+        $(this).delay(1000);
+        $('#nav2').animate({opacity: 100},500);
+    }
+    else {
+        $('#nav2').hide()
     }
     $('.button').on('mousedown', function() {
         $(this).css('border', '3px inset lightgray');
