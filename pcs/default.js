0a197e071aac4e80b6299f268eb0c55c92725881
Martin McNichols <mcflie1994@gmail.com>
2015-01-21 21:37:19 -0900

    Added  individual Projector Page to PCS (FC Stage Left)

--- pcs/default.js
+++ pcs/default.js
@@ -2,9 +2,11 @@
  * Created by mcflie1994 on 1/21/15.
  */
 $(document).ready(function() {
-    var pass = prompt('Please enter the password to access the ' + $('title').html());
-    if (pass != $('#pass').html()) {
-        window.location.replace('index.html');
+    if ($('#pass').html() != "" || $('pass').html() !== undefined) {
+        var pass = prompt('Please enter the password to access the ' + $('title').html());
+        if (pass != $('#pass').html()) {
+            window.location.replace('index.html');
+        }
     }
     $('.button').on('mousedown', function() {
         $(this).css('border', '3px inset lightgray');
