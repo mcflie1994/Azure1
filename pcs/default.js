85aff087bd006ce2c6daf14c74cc484df737320c
Martin McNichols <mcflie1994@gmail.com>
2015-01-22 12:30:41 -0900

    added link to admin and scripts to wc in PCS

--- pcs/default.js
+++ pcs/default.js
@@ -7,23 +7,26 @@ $(document).ready(function() {
         if (pass != $('#pass').html()) {
             window.location.replace('index.html');
         }
-    }
+    };
     if ($('#nav2').html() !== "" && $('#nav2').html() !== undefined) {
         $('#nav2').animate({marginTop: 0}, 1500, 'linear');
         $(this).delay(1000);
         $('#nav2').animate({opacity: 100},500);
     }
     else {
-        $('#nav2').hide()
-    }
+        $('#nav2').css('display','none')
+    };
+
     $('.button').on('mousedown', function() {
         $(this).css('border', '3px inset lightgray');
         $(this).on('mouseup', function() {
             $(this).css('border', '3px outset lightgray');
         });
     });
+
     $('.button').on('click', function() {
         $('#log').prepend('<span class="wrtout">' + $(this).html() + '</span><br />');
         $('#log').prepend('<span class="readin">' + $(this).html() + '</span><br />');
     });
+
 });
