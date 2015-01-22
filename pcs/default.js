25d51594921531016d5928073f2c5507f5654780
Martin McNichols <mcflie1994@gmail.com>
2015-01-21 23:50:07 -0900

    Added log area to VC in PCS

--- pcs/default.js
+++ pcs/default.js
@@ -2,7 +2,7 @@
  * Created by mcflie1994 on 1/21/15.
  */
 $(document).ready(function() {
-    if ($('#pass').html() != "" || $('pass').html() !== undefined) {
+    if ($('#pass').html() != "" || $('#pass').html() !== undefined) {
         var pass = prompt('Please enter the password to access the ' + $('title').html());
         if (pass != $('#pass').html()) {
             window.location.replace('index.html');
@@ -13,5 +13,9 @@ $(document).ready(function() {
         $(this).on('mouseup', function() {
             $(this).css('border', '3px outset lightgray');
         });
+    });
+    $('.button').on('click', function() {
+        $('#log').prepend('<span class="wrtout">' + $(this).html() + '</span><br />');
+        $('#log').prepend('<span class="readin">' + $(this).html() + '</span><br />');
     });
 });
